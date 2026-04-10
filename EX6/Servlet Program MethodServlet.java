570a6edb8be58b74b42fab46851ca34ec60cb555
Harrish501 <harrish.s.2024.aiml@rajalakshmi.edu.in>
2026-04-10 11:52:55 +0530

    Add files via upload
    Signed-off-by: Harrish501 <harrish.s.2024.aiml@rajalakshmi.edu.in>

---
+++ EX6/Servlet Program MethodServlet.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MethodServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        out.println("<html><body>");
+        out.println("<h2>GET Method Used</h2>");
+        out.println("<p>Name: " + name + "</p>");
+        out.println("<p>Email: " + email + "</p>");
+        out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        out.println("<html><body>");
+        out.println("<h2>POST Method Used</h2>");
+        out.println("<p>Name: " + name + "</p>");
+        out.println("<p>Email: " + email + "</p>");
+        out.println("</body></html>");
+    }
+}
